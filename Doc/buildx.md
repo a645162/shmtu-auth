9696f0434d1feef506cfc1023d3049015d7021fc
Haomin Kong <a645162@qq.com>
2024-03-10 00:21:05 +0800

    update CI

---
+++ Doc/buildx.md
@@ -0,0 +1,9 @@
+# Buildx
+
+
+## Ref
+
+https://docs.docker.com/build/ci/github-actions/configure-builder/
+https://juejin.cn/post/7000018934686547999
+https://blog.csdn.net/WZP_LOVE/article/details/121443240
+https://zhuanlan.zhihu.com/p/622399482
--- gengerate_docker_image_buildx.sh
+++ gengerate_docker_image_buildx.sh
@@ -5,6 +5,8 @@ find . -type d -name "__pycache__" -exec rm -r {} +
 
 rm -f ./shmtu_auth/logs/*
 
+docker buildx install
+
 docker buildx build \
   --progress=plain --no-cache \
   --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
