3cecb0d36ad42f73a786651b5008cb73e81ec85e
Haomin Kong <a645162@qq.com>
2024-03-14 15:42:41 +0800

    update Build "exe" Script.

--- Build/generate_exe.ps1
+++ Build/generate_exe.ps1
@@ -1,12 +1,28 @@
 # Save Current Loaction
-$oldLocation = Get-Location
+$baseLocation = Get-Location
 
-Set-Location .\shmtu_auth\
+$project_name= "shmtu_auth"
+
+$srcLocation = "$baseLocation\$project_name"
+$outputLocation = "$baseLocation\Build\$project_name"
+$tmpLocation = "$outputLocation\tmp"
+
+Set-Location $srcLocation
+
 pyinstaller `
     -F `
     -c `
-    -n shmtu_auth `
+    -s `
+    -n $project_name `
+    --distpath $outputLocation `
+    --workpath $tmpLocation `
     .\main_pyinstaller.py
 
+# Remove File "shmtu_auth.spec"
+Remove-Item -Recurse -Force "$srcLocation\$project_name.spec"
+
+# Remove Temp Files
+Remove-Item -Recurse -Force $tmpLocation
+
 # Restore Location
-Set-Location $oldLocation
+Set-Location $baseLocation
